c83a4033c27e36c41229474eab356d1d10ec760c
Jonan <jjsalomonzon@gmail.com>
2024-05-29 09:12:21 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // This is necessary for GitHub Pages
   server: {
     host: "localhost",
     port: 8080,
