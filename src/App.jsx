8246b354c7ab781630c806d13f047126be33eb03
Jonan <jjsalomonzon@gmail.com>
2024-06-03 20:17:18 -0700

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,6 @@ import "./App.module.scss";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./views/About";
 import Home from "./views/Home";
-import { Canvas } from "@react-three/fiber";
-import { css } from "./utils";
-import {
-  OrbitControls,
-  PerspectiveCamera,
-  useTexture,
-} from "@react-three/drei";
-
 import ELVTR from "./projects/ELVTR";
 
 function App() {
@@ -17,8 +9,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="elvtr" element={<ELVTR />} />
+        <Route path="about/" element={<About />} />
+        <Route path="elvtr/" element={<ELVTR />} />
       </Routes>
     </Router>
   );
