5f0023f79b69c3537e639cb2435af1cc849c4ace
Jonan <jjsalomonzon@gmail.com>
2024-06-03 20:27:28 -0700

    added # routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.module.scss";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./views/About";
 import Home from "./views/Home";
 import ELVTR from "./projects/ELVTR";
@@ -8,9 +8,9 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="about/" element={<About />} />
-        <Route path="elvtr/" element={<ELVTR />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/elvtr" element={<ELVTR />} />
       </Routes>
     </Router>
   );
